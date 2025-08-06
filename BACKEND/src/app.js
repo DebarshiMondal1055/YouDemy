a1b799f7a2d6c60d290c90cc9c6206f7fabf19d8
DebarshiMondal1055 <rishimondal99@gmail.com>
2025-08-07 02:08:48 +0530

    updated

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -6,12 +6,12 @@ import cookieParser from "cookie-parser"
 const app=express();
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin:true,
     credentials:true
 }))
 
-app.use(express.json({limit:"18kb"}))
-app.use(express.urlencoded({extended:true,limit:"18kb"}));
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({extended:true,limit:"50mb"}));
 app.use(express.static("public"))
 app.use(cookieParser())
 
